65899cda7906b2e970bddb45ff9f6b7093150e79
murugeshkumark <master4uall@gmail.com>
2020-04-10 14:24:46 -0400

    refactor the packages

---
+++ src/main/java/com/github/murugeshkumark/dsa/sorting/SelectionSortMain.java
@@ -0,0 +1,17 @@
+package com.github.murugeshkumark.dsa.sorting;
+
+public class SelectionSortMain {
+
+	public static void main(String[] args) {
+		int array[] = {10, 3, 2, 5, 8, 4, 3, 1, 2, 9, 7, 8};
+		
+		System.out.println("User entered Array: ");
+		SelectionSort.printArray(array);
+		
+		SelectionSort.selectionSort(array);
+		
+		System.out.println("\n\nAfter com.github.murugeshkumark.dsa.sorting: ");
+		SelectionSort.printArray(array);
+	}//end of method
+
+}//end of class
---
+++ src/main/java/com/github/murugeshkumark/dsa/tree/dfs/DFSRecursive.java
@@ -0,0 +1,47 @@
+package com.github.murugeshkumark.dsa.tree.dfs;
+import java.util.ArrayList;
+import com.github.murugeshkumark.dsa.node.GraphNode;
+
+public class DFSRecursive {
+	ArrayList<GraphNode> nodeList = new ArrayList<GraphNode>();
+
+	
+	public DFSRecursive(ArrayList<GraphNode> nodeList) {
+		this.nodeList = nodeList;
+	}
+	
+	
+	void dfs() {
+		//if a com.github.murugeshkumark.dsa.node is unvisited then run com.github.murugeshkumark.dsa.tree.dfs on it
+		for(GraphNode node: nodeList) {
+			if(!node.isVisited())
+				dfsVisit(node);
+		}
+	}
+	
+	
+	//com.github.murugeshkumark.dsa.tree.dfs traversal by a source com.github.murugeshkumark.dsa.node
+	void dfsVisit(GraphNode node) {
+		//mark com.github.murugeshkumark.dsa.node as visited
+		node.setVisited(true);
+		//print the com.github.murugeshkumark.dsa.node
+		System.out.print(node.getName()+" ");
+		//for each neighbor of present com.github.murugeshkumark.dsa.node
+		for(GraphNode neighbor: node.getNeighbors()) {
+			//if neighbor is not visited 
+			if(!neighbor.isVisited()) {
+				//recursive call to com.github.murugeshkumark.dsa.tree.dfs function
+				dfsVisit(neighbor);
+			}
+		}
+	}
+	
+	// add an undirected edge between two nodes
+	public void addUndirectedEdge(int i, int j) {
+		GraphNode first = nodeList.get(i-1);
+		GraphNode second = nodeList.get(j-1);
+		first.getNeighbors().add(second);
+		second.getNeighbors().add(first);
+	}
+	
+}
